cab1a259d4adb620a38717816952819f558b7cf5
Nick <nchemsak@gmail.com>
2017-03-21 13:43:37 -0500

    example creds added

---
+++ ExampleCreds.py
@@ -0,0 +1,14 @@
+##PUT ALL INFO BETWEEN QUOTES BELOW AND RENAME THIS FILE TO 'creds.py'
+
+#Twilio API Account info
+ACCOUNT_SID = ""
+AUTH_TOKEN = ""
+
+# your cell phone number below (must begin with '+', example: "+15555555555")
+TO_PHONE = ""
+
+# your Twilio phone number below (must begin with '+', example: "+15555555555")
+FROM_PHONE = ""
+
+# Imgur ID
+CLIENT_ID = ""
