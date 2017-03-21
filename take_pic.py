71460969f88b02467ff939531877225371fb419d
Nick Chemsak <nchemsak@gmail.com>
2017-03-21 13:30:11 -0500

    datetime to photos

--- take_pic.py
+++ take_pic.py
@@ -1,16 +1,24 @@
 import picamera
+import datetime
 
 IMG_WIDTH = 800
 IMG_HEIGHT = 600
-#IMAGE_DIR = "doorbell_dash_project/photos/photos/"
+#IMAGE_DIR = "/doorbell_dash_project/photos/photos"
 IMAGE_DIR = "/home/pi/Desktop/"
 IMG = "snap.jpg"
+#suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S.jpg")
+#file = "_".join([IMAGE_DIR, suffix])
 
-def pic():   
+
+def pic():
     camera = picamera.PiCamera()
     camera.vflip = True
     camera.hflip = True
     camera.brightness = 60
     camera.resolution = (IMG_WIDTH, IMG_HEIGHT)
     camera.capture(IMAGE_DIR + IMG)
+    #camera.capture(file)
     camera.close()
+
+
+
